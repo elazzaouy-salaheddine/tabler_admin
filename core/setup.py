4a4f21e4d78ef2a226d289ad328d1e6f2f10a0d9
salaheddine.elazzaouy <salaheddine.elazzaouy@outlook.com>
2023-10-08 13:20:03 +0100

    update requirements

---
+++ core/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tabler_django',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'Django',
+        # Add other dependencies here
+    ],
+)
